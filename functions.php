b0e1842e00766d5c29d3e1b543b98d0ce4909181
garikhg <garik.28h@gmail.com>
2024-04-10 21:05:35 +0400

    Define WPKIT_VERSION constant in functions.php
    A 'WPKIT_VERSION' constant has been defined in the functions.php file. This was done to standardize the versioning system in wpkit theme.

--- functions.php
+++ functions.php
@@ -7,6 +7,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+define('WPKIT_VERSION', '3.0.2');
+
 /** Include template functions */
 require_once get_template_directory() . '/inc/setup.php';
 
